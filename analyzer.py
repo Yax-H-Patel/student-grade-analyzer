51f9717bf1ada77ba4afe729061236701cf18d3f
Yax Patel <yaxp9951@gmail.com>
2025-10-28 16:36:16 -0600

    Implemented calculate_grade() function and modified read_students()

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,18 @@
 def read_students(filename):
-    file = open("filename", 'r')
-    return file.readlines()
+    file = open(filename, 'r')
+    data = []
+    for line in file.readlines():
+        data.append([line.split(',')[0], int(line.split(',')[1].strip())])
+    return data
+
+def calculate_grade(mark):
+    if mark > 90:
+        return 'A'
+    elif mark > 80:
+        return 'B'
+    elif mark > 70:
+        return 'C'
+    elif mark > 60:
+        return 'D'
+    else:
+        return 'F'
